626d3ab70085f00a67f46e1e8683cd5f0afcbf2a
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-25 14:45:04 +0500

    add:/ main navigation container added

--- src/modules/onboard/index.tsx
+++ src/modules/onboard/index.tsx
@@ -1,14 +1,12 @@
-
-
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 export default function Splash() {
-  return (
-    <View>
-      <Text>Splash</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text>Splash</Text>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Splash from '@modules/onboard';
+import Home from '@modules/home';
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
