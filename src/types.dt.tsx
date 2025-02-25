9977c856d8b5c222b4cf8b7270a32dfa7303b3ea
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-25 13:42:32 +0500

    add:/ types added for files

---
+++ src/types.dt.tsx
@@ -0,0 +1,11 @@
+
+declare module "*.png"
+declare module "*.jpg"
+declare module "*.jpeg"
+declare module "*.mp3"
+declare module "*.obj"
+declare module "*.virx"
+declare module "*.bin"
+declare module "*.glb"
+declare module "*.mtl"
+
