f3442f1a39d77a3dba865232a680f21686a1b041
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-25 16:12:02 +0500

    Redux Storage helpers added

---
+++ src/store/storage.tsx
@@ -0,0 +1,18 @@
+import {MMKV} from 'react-native-mmkv';
+
+const storage = new MMKV();
+
+export const reduxStorage = {
+  setItem: (key: string, value: any) => {
+    storage.set(key, value);
+    return Promise.resolve(true);
+  },
+  getItem: (key: string) => {
+    const value = storage.getString(key);
+    return Promise.resolve(value);
+  },
+  removeItem: (key: string) => {
+    storage.delete(key);
+    return Promise.resolve(true);
+  },
+};
