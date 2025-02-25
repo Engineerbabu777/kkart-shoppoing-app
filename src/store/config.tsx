d62787a13c17138cb7e13dbe489f5cb61500bc7b
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-25 16:20:13 +0500

    Congig file for hosts

---
+++ src/store/config.tsx
@@ -0,0 +1,6 @@
+
+// const VIRTUAL_DEVICE_HOST =
+//   Platform.OS === 'android' ? '10.0.2.2' : '192.168.154.60';
+const PHYSICAL_DEVICE_HOST = '192.168.154.60';
+
+export const BASE_URL = `http://${PHYSICAL_DEVICE_HOST}:3000`;
