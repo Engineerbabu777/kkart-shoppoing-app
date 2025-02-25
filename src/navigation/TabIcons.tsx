bd3d22779716a15afbec11eaa70fe5574162beba
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-25 15:57:40 +0500

    add:/ Clean Tab Icons Component Done

--- src/navigation/TabIcons.tsx
+++ src/navigation/TabIcons.tsx
@@ -1,3 +1,6 @@
+import Icon from '@components/atoms/Icon';
+import React from 'react';
+
 interface TabIconProps {
   focused: boolean;
   size: number;
@@ -5,5 +8,57 @@ interface TabIconProps {
 }
 
 export const HomeIcon: React.FC<TabIconProps> = ({color, focused, size}) => {
-  return <></>;
+  return (
+    <>
+      <Icon
+        name={focused ? 'home' : 'home-outline'}
+        size={size}
+        color={color}
+        iconFamily="Ionicons"
+      />
+    </>
+  );
+};
+
+export const CategoriesIcon: React.FC<TabIconProps> = ({
+  color,
+  focused,
+  size,
+}) => {
+  return (
+    <>
+      <Icon
+        name={focused ? 'grid' : 'grid-outline'}
+        size={size}
+        color={color}
+        iconFamily="Ionicons"
+      />
+    </>
+  );
+};
+
+export const AccountIcon: React.FC<TabIconProps> = ({color, focused, size}) => {
+  return (
+    <>
+      <Icon
+        name={focused ? 'person' : 'person-outline'}
+        size={size}
+        color={color}
+        iconFamily="Ionicons"
+      />
+    </>
+  );
+};
+
+export const CartIcon: React.FC<TabIconProps> = ({color, focused, size}) => {
+  return (
+    <>
+      <Icon
+        name={focused ? 'cart' : 'cart-outline'}
+        size={size}
+        color={color}
+        iconFamily="MaterialCommunityIcons"
+      />
+    </>
+  );
 };
