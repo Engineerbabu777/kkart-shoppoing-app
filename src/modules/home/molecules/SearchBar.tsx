39c55d56360027f05e4cc7235eb2e5e893ff570d
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-27 14:45:29 +0500

    Rooling Search Bar added

--- src/modules/home/molecules/SearchBar.tsx
+++ src/modules/home/molecules/SearchBar.tsx
@@ -1,12 +1,55 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 import {Colors} from '@utils/Constants';
 import {RFValue} from 'react-native-responsive-fontsize';
+import Icon from '@components/atoms/Icon';
+import RollingContent from 'react-native-rolling-bar';
+import {searchItems} from '@utils/db';
 
 export default function SearchBar() {
+  const [isOn, setIsOn] = useState(false);
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+  };
   return (
     <>
       <SafeAreaView />
+
+      <View style={styles.container}>
+        <Pressable onPress={toggleSwitch} style={styles.toggleContainer}>
+          <Text style={styles.brandText}>Brand Mall</Text>
+          <Image
+            source={
+              isOn
+                ? require('@assets/icons/switch_on.png')
+                : require('@assets/icons/switch_off.png')
+            }
+            style={styles.switchIcon}
+          />
+        </Pressable>
+        <Pressable style={styles.searchContainer}>
+          <Icon name="search" iconFamily="Ionicons" size={20} color="black" />
+          <RollingContent
+            interval={3000}
+            defaultStyle={false}
+            customStyle={styles.textContainer}>
+            {searchItems.map((searchItem: string) => (
+              <Text
+                key={searchItem}
+                style={{fontSize: RFValue(10), color: Colors.text}}>
+                {searchItem}
+              </Text>
+            ))}
+          </RollingContent>
+        </Pressable>
+      </View>
     </>
   );
 }
